d6ea4c7da72537bbdea1caeeb388404188094fca
manunasci <nascimentomanuella73@gmail.com>
2021-09-17 22:17:55 -0300

    Adiciona o calculo do juros e montante

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu salario: ");
+            string valordoSalario = Console.ReadLine();
+            double numerodoSalario = Double.Parse(valordoSalario);
+
+            Console.WriteLine();
+
+            Console.Write("Digite a taxa do juros: ");
+            string valordaTaxa = Console.ReadLine();
+            double numerodaTaxa = Double.Parse(valordaTaxa);
+
+            Console.WriteLine();
+
+            Console.Write("Digite o tempo em meses: ");
+            string valordoTempo = Console.ReadLine();
+            double numerodoTempo= Double.Parse(valordoTempo);
+
+            double valordoJuros = numerodoSalario*(numerodaTaxa/100)*numerodoTempo;
+            double valordoMontante = numerodoSalario + valordoJuros;
+
+            Console.WriteLine();
+
+            Console.WriteLine("Juros (R$): " + valordoJuros);
+            Console.WriteLine("Montante (R$): " + valordoMontante);
+        }
+    }
+}
